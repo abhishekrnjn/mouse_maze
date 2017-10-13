ffc16bb2b47dcc11a1980f5dc37fbe223328ee76
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2017-10-13 22:26:41 +0530

    change color of first when cursor touches this div

--- maze.js
+++ maze.js
@@ -1,4 +1,4 @@
 function changecolor(){
 	document.getElementById("boundary1").style.backgroundColor="red";
-	alert("you touched the boundary please try again!!!!");
+	alert("you touched the boundary try again");
 }
