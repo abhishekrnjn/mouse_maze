8d30d9c3027d4341d39d9bc88229c3ef7cca015e
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2017-10-16 10:40:41 +0530

    added timer functionality

--- maze.js
+++ maze.js
@@ -1,3 +1,41 @@
+var alerted =false;// used to remove multiple alert
+var stclick=false;// stores value to check player clicked start or not
+var stime;// stores value when S is clicked
+
+
+//start function is invoked when S is clicked
+function start(){
+	mousecur=true;
+	stclick=true;
+	stime=Date.now();
+	ondiv=true;
+var x= document.getElementsByClassName("boundary");
+var i;
+for(i=0;i<x.length;i++){
+	x[i].style.backgroundColor="#eeeeee";
+}
+    if(alerted==true) {
+		window.alerted=false;
+		
+}
+}
+
+
+// end function is invoked when E is clicked
+function end(){
+
+	if(stclick && mousecur){
+		var etime=Date.now();
+		var tgap=etime-stime;
+		var tgaps=tgap/parseFloat(1000);
+		alert("Yeah!!, You completed in:"+ tgaps+"s");
+
+	}
+
+}
+
+
+
 
 function changecolor(){
 
