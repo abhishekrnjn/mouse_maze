1d075c6a7be21d786e8b3e408c67d868bb5a8c83
Abhishek Ranjan <abhishek.ranjan2690@gmail.com>
2017-10-16 10:34:18 +0530

    change color of complete maze when touched boundary

--- maze.js
+++ maze.js
@@ -1,4 +1,23 @@
+
+function changecolor(){
+
+var x= document.getElementsByClassName("boundary");
+var i;
+for(i=0;i<x.length;i++){
+	x[i].style.backgroundColor="red";
+	
+}
+
+    if (!window.alerted) {
+window.alert("Sorry!! you lost :( ");
+	window.alerted=true;}
+}
+
+
+
+/*
 function changecolor(){
 	document.getElementById("boundary1").style.backgroundColor="red";
 	alert("you touched the boundary try again");
 }
+*/
